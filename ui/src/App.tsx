37e6f3fc16cebc294d13486c98c665a817ba5c72
Tejas Kumar <tejas@tejas.qa>
2024-10-25 16:29:41 +0530

    Add Sentry

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { PostHogProvider } from "posthog-js/react";
+import * as Sentry from "@sentry/react";
 
 import { ChooseTeam } from "./ChooseTeam";
 import { Registration } from "./Registration";
@@ -10,6 +11,21 @@ import { InGame } from "./InGame";
 import { Admin } from "./Admin";
 import { GameOver } from "./GameOver";
 import { preventSleep } from "./util/preventSleep";
+
+Sentry.init({
+  dsn: "https://b49a717d19bf879220ef6dd8bf66d421@o4508182996910080.ingest.us.sentry.io/4508182998089728",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const options = {
   api_host: import.meta.env.VITE_POSTHOG_HOST,
