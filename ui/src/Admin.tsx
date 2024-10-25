592a0c57f3c407a994c774ae311cf0ec0e762b17
Tejas Kumar <tejas@tejas.qa>
2024-10-25 14:24:53 +0530

    Admin aesthetic

--- ui/src/Admin.tsx
+++ ui/src/Admin.tsx
@@ -1,12 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParty } from "./PartyContext";
 import { Team, Quote } from "../../common/types";
+import { teamBgColors } from "./util/teamBgColors";
+import { PlayerCount } from "./PlayerCount";
 
 export function Admin() {
   const [teams, setTeams] = useState<Record<string, Team>>({});
   const [currentQuote, setCurrentQuote] = useState<Quote | null>(null);
   const [allQuotes, setAllQuotes] = useState<Quote[]>([]);
   const { ws } = useParty();
+  const quotesContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (!ws) return;
@@ -50,6 +53,19 @@ export function Admin() {
     };
   }, [ws]);
 
+  useEffect(() => {
+    if (currentQuote && quotesContainerRef.current) {
+      const currentQuoteElement =
+        quotesContainerRef.current.querySelector(".border-ds-primary");
+      if (currentQuoteElement) {
+        currentQuoteElement.scrollIntoView({
+          behavior: "smooth",
+          inline: "center",
+        });
+      }
+    }
+  }, [currentQuote]);
+
   const handleStartGame = () => {
     if (ws) {
       ws.send(JSON.stringify({ type: "startGame" }));
@@ -68,104 +84,163 @@ export function Admin() {
     }
   };
 
+  const totalPlayers = Object.values(teams).reduce(
+    (acc, team) => acc + team.players.length,
+    0
+  );
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Admin Panel</h1>
-      <button
-        onClick={handleStartGame}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
-      >
-        Start Game
-      </button>
-      <button
-        onClick={handleSendNextQuote}
-        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2"
-      >
-        Send Next Quote
-      </button>
-      <button
-        onClick={handleResetGame}
-        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Reset Game
-      </button>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold mb-2">Quotes:</h2>
-        {allQuotes.map((quote, index) => (
-          <div
-            key={index}
-            className={`bg-gray-100 p-4 rounded mb-2 ${
-              currentQuote && currentQuote.quote === quote.quote
-                ? "border-2 border-blue-500"
-                : ""
-            }`}
+    <div className="p-8 grid gap-8">
+      <h1 className="text-6xl font-bold">Game Management</h1>
+      <fieldset className="border flex items-center gap-2 justify-between rounded p-4 border-neutral-800">
+        <div className="text-lg">{totalPlayers} players connected.</div>
+        <div className="flex items-center gap-2">
+          <button
+            onClick={handleSendNextQuote}
+            className="border border-white text-white font-bold py-2 px-4 rounded mr-2"
           >
-            <p className="font-bold">{quote.quote}</p>
-            <ul className="list-disc list-inside">
-              {quote.options.map((option, optionIndex) => (
-                <li
-                  key={optionIndex}
-                  className={
-                    optionIndex === quote.correctOptionIndex
-                      ? "text-green-600 font-bold"
-                      : ""
-                  }
-                >
-                  {option}
-                </li>
-              ))}
-            </ul>
+            Send Next Quote
+          </button>
+          <button
+            onClick={handleResetGame}
+            className="border-red-500 border text-red-400 font-bold py-2 px-4 rounded"
+          >
+            Reset Game
+          </button>
+        </div>
+      </fieldset>
+      <div className="grid gap-4">
+        <h2 className="text-4xl font-semibold">Quotes</h2>
+        {allQuotes.length === 0 ? (
+          <div className="border border-neutral-800 p-4 rounded grid gap-2 items-center justify-center">
+            <span>No quotes yet.</span>
+            <button
+              onClick={handleStartGame}
+              className="bg-white text-black font-bold py-2 px-4 rounded"
+            >
+              Start Game
+            </button>
           </div>
-        ))}
-      </div>
-      <div className="mb-4">
-        <h2 className="text-xl font-semibold mb-2">Teams:</h2>
-        {Object.entries(teams).map(([teamId, teamData]) => (
-          <div key={teamId} className="mb-4">
-            <h3 className="font-medium mb-2">
-              Team {teamId} - Score: {teamData.score}
-            </h3>
-            <div className="overflow-x-auto">
-              <table className="w-full border-collapse border border-gray-300">
-                <thead>
-                  <tr className="bg-gray-100">
-                    <th className="border border-gray-300 px-4 py-2">Player</th>
-                    {allQuotes.map((_, index) => (
-                      <th
-                        key={index}
-                        className="border border-gray-300 px-4 py-2"
+        ) : (
+          <div className="overflow-x-auto">
+            <div ref={quotesContainerRef} className="flex space-x-4">
+              {allQuotes.map((quote, index) => (
+                <div
+                  key={index}
+                  className={`border grid gap-1 p-4 rounded flex-shrink-0 w-64 ${
+                    currentQuote && currentQuote.quote === quote.quote
+                      ? "border-2 border-ds-primary"
+                      : "border-neutral-800"
+                  }`}
+                >
+                  <p className="font-bold">&ldquo;{quote.quote}&rdquo;</p>
+                  <ul className="list-disc list-inside">
+                    {quote.options.map((option, optionIndex) => (
+                      <li
+                        key={optionIndex}
+                        className={
+                          optionIndex === quote.correctOptionIndex
+                            ? "text-ds-quaternary font-bold"
+                            : ""
+                        }
                       >
-                        Round {index + 1}
-                      </th>
+                        {option}
+                      </li>
                     ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {teamData.players.map((player) => (
-                    <tr key={player.id}>
-                      <td className="border border-gray-300 px-4 py-2">
-                        {player.email}
-                      </td>
-                      {allQuotes.map((_, quoteIndex) => {
-                        const choice = player.choices[quoteIndex];
-                        return (
-                          <td
-                            key={`${teamId}-${player.id}-${quoteIndex}`}
-                            className="border border-gray-300 px-4 py-2"
-                          >
-                            {choice
-                              ? `${choice.value} (${choice.status})`
-                              : `N/A (undecided)`}
-                          </td>
-                        );
-                      })}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </ul>
+                </div>
+              ))}
             </div>
           </div>
-        ))}
+        )}
+      </div>
+      <div className="grid gap-4">
+        <h2 className="text-4xl font-semibold">Teams</h2>
+        <div className="grid gap-4 grid-cols-2">
+          {Object.entries(teams).map(([teamId, teamData]) => (
+            <div
+              key={teamId}
+              className={`bg-${teamBgColors[teamId]} grid gap-4 rounded p-4`}
+            >
+              <h3 className="flex items-center justify-between">
+                <span>Team {teamId}</span>
+                <div className="flex items-center gap-2">
+                  <PlayerCount count={teamData.players.length} />
+                  <span>•</span>
+                  <span>Score: {teamData.score}</span>
+                </div>
+              </h3>
+              {allQuotes.length > 0 && (
+                <ul className="list-disc list-inside">
+                  {Array.from({ length: allQuotes.length }, (_, roundIndex) => (
+                    <li key={roundIndex}>
+                      <span className="font-bold">Round {roundIndex + 1}:</span>{" "}
+                      {teamData.players.some(
+                        (player) =>
+                          player.choices[roundIndex]?.status === "accepted"
+                      )
+                        ? (() => {
+                            const acceptedPlayer = teamData.players.find(
+                              (player) =>
+                                player.choices[roundIndex]?.status ===
+                                "accepted"
+                            );
+                            return `${acceptedPlayer?.choices[roundIndex]?.value} (${acceptedPlayer?.email})`;
+                          })()
+                        : "No accepted answer"}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <h2 className="text-4xl font-semibold">Players</h2>
+        <table className="w-full border-collapse border rounded">
+          <thead>
+            <tr className="bg-neutral-800">
+              <th className="border border-neutral-800 p-2">Email</th>
+              {Array.from({ length: allQuotes.length }, (_, index) => (
+                <th key={index} className="border border-neutral-800 p-2">
+                  Round {index + 1}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {totalPlayers === 0 ? (
+              <tr>
+                <td className="p-4 border-neutral-800 border text-center">
+                  No players yet.
+                </td>
+              </tr>
+            ) : (
+              Object.values(teams).flatMap((team) =>
+                team.players.map((player) => (
+                  <tr key={player.email} className="border-b">
+                    <td className="border border-neutral-800 p-2">
+                      {player.email}
+                    </td>
+                    {Array.from(
+                      { length: allQuotes.length },
+                      (_, roundIndex) => (
+                        <td
+                          key={roundIndex}
+                          className="border border-neutral-800 p-2"
+                        >
+                          {player.choices[roundIndex]
+                            ? `${player.choices[roundIndex].value} (${player.choices[roundIndex].status})`
+                            : "No choice"}
+                        </td>
+                      )
+                    )}
+                  </tr>
+                ))
+              )
+            )}
+          </tbody>
+        </table>
       </div>
     </div>
   );
--- ui/src/PartyContext.tsx
+++ ui/src/PartyContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import { usePartySocket } from "partysocket/react";
 import PartySocket from "partysocket";
 
@@ -7,6 +7,7 @@ const PartyContext = createContext<{ ws: PartySocket | null }>({ ws: null });
 export const useParty = () => useContext(PartyContext);
 
 export const PartyProvider = ({ children }: { children: React.ReactNode }) => {
+  const [error, setError] = useState<boolean | null>(false);
   let email = "";
 
   try {
@@ -19,7 +20,21 @@ export const PartyProvider = ({ children }: { children: React.ReactNode }) => {
     host: import.meta.env.VITE_PARTYKIT_HOST,
     room: "lobby",
     ...(email && { id: email }),
+    onError: () => {
+      setError(true);
+    },
   });
+
+  if (error) {
+    return (
+      <div className="p-8 h-svh">
+        <div className="rounded text-2xl grid gap-4 content-center text-center bg-ds-primary h-full text-white p-8">
+          <h1 className="text-6xl font-bold">Oh no!</h1>
+          <p>Cannot connect to the game server! Please ask a host about it.</p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <PartyContext.Provider value={{ ws }}>{children}</PartyContext.Provider>
