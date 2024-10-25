36e204825f3affa3dc4c9f0e566b6d78cdea2a08
Tejas Kumar <tejas@tejas.qa>
2024-10-25 14:30:54 +0530

    Add analytics

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import { PostHogProvider } from "posthog-js/react";
+
 import { ChooseTeam } from "./ChooseTeam";
 import { Registration } from "./Registration";
 import { TeamRoom } from "./TeamRoom";
@@ -6,8 +9,11 @@ import { PartyProvider } from "./PartyContext";
 import { InGame } from "./InGame";
 import { Admin } from "./Admin";
 import { GameOver } from "./GameOver";
-import { useEffect } from "react";
 import { preventSleep } from "./util/preventSleep";
+
+const options = {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+};
 
 function App() {
   useEffect(() => {
@@ -15,16 +21,21 @@ function App() {
   }, []);
 
   return (
-    <PartyProvider>
-      <Routes>
-        <Route path="/" element={<ChooseTeam />} />
-        <Route path="/registration/:teamId" element={<Registration />} />
-        <Route path="/team/:teamId" element={<TeamRoom />} />
-        <Route path="/game/:teamId" element={<InGame />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/game-over/:teamId" element={<GameOver />} />
-      </Routes>
-    </PartyProvider>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_POSTHOG_KEY}
+      options={options}
+    >
+      <PartyProvider>
+        <Routes>
+          <Route path="/" element={<ChooseTeam />} />
+          <Route path="/registration/:teamId" element={<Registration />} />
+          <Route path="/team/:teamId" element={<TeamRoom />} />
+          <Route path="/game/:teamId" element={<InGame />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/game-over/:teamId" element={<GameOver />} />
+        </Routes>
+      </PartyProvider>
+    </PostHogProvider>
   );
 }
 
