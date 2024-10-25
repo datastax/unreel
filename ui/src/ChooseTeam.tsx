8c1c6d74f667369a47fd8748c9b5b7deaeea6c92
Tejas Kumar <tejas@tejas.qa>
2024-10-25 17:31:11 +0530

    Fix some branding

--- ui/src/ChooseTeam.tsx
+++ ui/src/ChooseTeam.tsx
@@ -5,6 +5,7 @@ import { PlayerCount } from "./PlayerCount";
 import { teamBgColors } from "./util/teamBgColors";
 import { useParty } from "./PartyContext";
 import { maxPlayersPerTeam, type Team } from "../../common/types";
+import { DataStax } from "./DataStax";
 
 export function ChooseTeam() {
   const [playersByTeam, setPlayersByTeam] = useState<Record<string, Team>>({});
@@ -27,23 +28,28 @@ export function ChooseTeam() {
   }, [ws]);
 
   return (
-    <div className="grid p-4 gap-4 h-svh w-svw items-center justify-center content-center text-center grid-rows-2 grid-cols-2">
-      {Object.entries(playersByTeam).map(([name, team]) => (
-        <Link
-          key={name}
-          className={`${
-            team.players.length >= maxPlayersPerTeam
-              ? "pointer-events-none opacity-50"
-              : ""
-          } rounded grid gap-2 h-full content-center items-center justify-center text-white text-3xl font-bold bg-${
-            teamBgColors[name]
-          }`}
-          to={`/registration/${name}`}
-        >
-          Team {name}
-          <PlayerCount count={team.players.length} />
-        </Link>
-      ))}
-    </div>
+    <main className="p-4 grid grid-rows-[auto_1fr] gap-4 h-svh w-svw">
+      <div>
+        <DataStax />
+      </div>
+      <div className="grid gap-4 w-full h-full items-center justify-center content-center text-center grid-rows-2 grid-cols-2">
+        {Object.entries(playersByTeam).map(([name, team]) => (
+          <Link
+            key={name}
+            className={`${
+              team.players.length >= maxPlayersPerTeam
+                ? "pointer-events-none opacity-50"
+                : ""
+            } rounded grid gap-2 h-full content-center items-center justify-center text-white text-3xl font-bold bg-${
+              teamBgColors[name]
+            }`}
+            to={`/registration/${name}`}
+          >
+            Team {name}
+            <PlayerCount count={team.players.length} />
+          </Link>
+        ))}
+      </div>
+    </main>
   );
 }
