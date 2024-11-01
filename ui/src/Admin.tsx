ef41cff75c92e6e47033a5c30fdc8f81b42db13c
Tejas Kumar <tejas.kumar@datastax.com>
2024-11-01 16:11:42 +0530

    RWD Admin UI

--- ui/src/Admin.tsx
+++ ui/src/Admin.tsx
@@ -63,20 +63,20 @@ export function Admin() {
     0
   );
   return (
-    <div className="p-8 grid gap-8">
-      <h1 className="text-6xl font-bold">Game Management</h1>
-      <fieldset className="border flex items-center gap-2 justify-between rounded p-4 border-neutral-800">
+    <div className="md:p-8 p-4 grid gap-8">
+      <h1 className="md:text-6xl text-4xl font-bold">Game Management</h1>
+      <fieldset className="border flex flex-col md:flex-row items-center gap-2 justify-between rounded p-4 border-neutral-800">
         <div className="text-lg">{totalPlayers} players connected.</div>
         <div className="flex items-center gap-2">
           <button
             onClick={handleSendNextQuote}
-            className="border border-white text-white font-bold py-2 px-4 rounded mr-2"
+            className="md:text-base text-sm border border-white text-white font-bold py-2 px-4 rounded mr-2"
           >
             Send Next Quote
           </button>
           <button
             onClick={handleResetGame}
-            className="border-red-500 border text-red-400 font-bold py-2 px-4 rounded"
+            className="md:text-base text-sm border-red-500 border text-red-400 font-bold py-2 px-4 rounded"
           >
             Reset Game
           </button>
@@ -129,7 +129,7 @@ export function Admin() {
       </div>
       <div className="grid gap-4">
         <h2 className="text-4xl font-semibold">Teams</h2>
-        <div className="grid gap-4 grid-cols-2">
+        <div className="grid gap-4 md:grid-cols-2">
           {Object.entries(teams).map(([teamId, teamData]) => (
             <div
               key={teamId}
@@ -171,50 +171,59 @@ export function Admin() {
       </div>
       <div className="grid gap-4">
         <h2 className="text-4xl font-semibold">Players</h2>
-        <table className="w-full border-collapse border rounded">
-          <thead>
-            <tr className="bg-neutral-800">
-              <th className="border border-neutral-800 p-2">Email</th>
-              {Array.from({ length: allQuotes.length }, (_, index) => (
-                <th key={index} className="border border-neutral-800 p-2">
-                  Round {index + 1}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {totalPlayers === 0 ? (
-              <tr>
-                <td className="p-4 border-neutral-800 border text-center">
-                  No players yet.
-                </td>
+        <div className="overflow-x-auto">
+          <table className="w-full border-collapse border rounded">
+            <thead>
+              <tr className="bg-neutral-800">
+                <th className="border border-neutral-800 p-2">Team</th>
+                <th className="border border-neutral-800 p-2">Email</th>
+                {Array.from({ length: allQuotes.length }, (_, index) => (
+                  <th key={index} className="border border-neutral-800 p-2">
+                    Round {index + 1}
+                  </th>
+                ))}
               </tr>
-            ) : (
-              Object.values(teams).flatMap((team) =>
-                team.players.map((player) => (
-                  <tr key={player.email} className="border-b">
-                    <td className="border border-neutral-800 p-2">
-                      {player.email}
-                    </td>
-                    {Array.from(
-                      { length: allQuotes.length },
-                      (_, roundIndex) => (
-                        <td
-                          key={roundIndex}
-                          className="border border-neutral-800 p-2"
-                        >
-                          {player.choices[roundIndex]
-                            ? `${player.choices[roundIndex].value} (${player.choices[roundIndex].status})`
-                            : "No choice"}
-                        </td>
-                      )
-                    )}
-                  </tr>
-                ))
-              )
-            )}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {totalPlayers === 0 ? (
+                <tr>
+                  <td
+                    colSpan={allQuotes.length + 2}
+                    className="p-4 border-neutral-800 border text-center"
+                  >
+                    No players yet.
+                  </td>
+                </tr>
+              ) : (
+                Object.entries(teams).flatMap(([teamId, team]) =>
+                  team.players.map((player) => (
+                    <tr key={player.email} className="border-b">
+                      <td className="border border-neutral-800 p-2">
+                        Team {teamId}
+                      </td>
+                      <td className="border border-neutral-800 p-2">
+                        {player.email}
+                      </td>
+                      {Array.from(
+                        { length: allQuotes.length },
+                        (_, roundIndex) => (
+                          <td
+                            key={roundIndex}
+                            className="border border-neutral-800 p-2"
+                          >
+                            {player.choices[roundIndex]
+                              ? `${player.choices[roundIndex].value} (${player.choices[roundIndex].status})`
+                              : "No choice"}
+                          </td>
+                        )
+                      )}
+                    </tr>
+                  ))
+                )
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
