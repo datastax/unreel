2b4f6a8952ea9bb6817db2a9b92a25bd95c781df
Phil Nash <philnash@gmail.com>
2024-11-06 18:15:09 +1100

    Remove debugging state from leaderboard

--- ui/src/Leaderboard.tsx
+++ ui/src/Leaderboard.tsx
@@ -65,116 +65,113 @@ export function Leaderboard() {
   }
 
   return (
-    <>
-      <main className="p-4 grid grid-rows-[auto_1fr_1fr] gap-4 h-svh w-svw">
-        <div>
-          <DataStax />
-        </div>
+    <main className="p-4 grid grid-rows-[auto_1fr_1fr] gap-4 h-svh w-svw">
+      <div>
+        <DataStax />
+      </div>
 
-        {gameEndedAt && winningTeams.length > 0 && (
-          <>
-            <Confetti colors={["#7f3aa4", "#ac115f", "#de2337", "#ffca0b"]} />
-            <h2 className="text-3xl font-bold pb-4 pt-4 text-center">
-              {winningTeams.length > 1 ? "The winners are:" : "The winner is:"}
-            </h2>
+      {gameEndedAt && winningTeams.length > 0 && (
+        <>
+          <Confetti colors={["#7f3aa4", "#ac115f", "#de2337", "#ffca0b"]} />
+          <h2 className="text-3xl font-bold pb-4 pt-4 text-center">
+            {winningTeams.length > 1 ? "The winners are:" : "The winner is:"}
+          </h2>
 
-            <ol>
-              {winningTeams.map((team) => (
-                <li
-                  key={team.id}
-                  className={`rounded text-center text-3xl font-bold bg-${
-                    teamBgColors[team.id]
-                  } mb-2 h-24 flex items-center p-4`}
-                >
-                  <p className="flex-grow text-left">Team {team.id}</p>
-                  <p>{team.score} pts</p>
-                </li>
-              ))}
+          <ol>
+            {winningTeams.map((team) => (
+              <li
+                key={team.id}
+                className={`rounded text-center text-3xl font-bold bg-${
+                  teamBgColors[team.id]
+                } mb-2 h-24 flex items-center p-4`}
+              >
+                <p className="flex-grow text-left">Team {team.id}</p>
+                <p>{team.score} pts</p>
+              </li>
+            ))}
 
-              {otherTeams.map((team) => (
-                <li
-                  key={team.id}
-                  className={`rounded text-center text-xl font-bold bg-${
-                    teamBgColors[team.id]
-                  } mb-2 h-18 flex items-center p-4`}
-                >
-                  <p className="flex-grow text-left">Team {team.id}</p>
-                  <p>{team.score} pts</p>
-                </li>
-              ))}
-            </ol>
-          </>
-        )}
+            {otherTeams.map((team) => (
+              <li
+                key={team.id}
+                className={`rounded text-center text-xl font-bold bg-${
+                  teamBgColors[team.id]
+                } mb-2 h-18 flex items-center p-4`}
+              >
+                <p className="flex-grow text-left">Team {team.id}</p>
+                <p>{team.score} pts</p>
+              </li>
+            ))}
+          </ol>
+        </>
+      )}
 
-        {!gameEndedAt && (
-          <>
-            {!gameStarted && (
-              <div className="self-center">
-                <div>
-                  <p className="text-center text-2xl mb-8">
-                    Join the game at{" "}
-                    <span className="text-3xl text-ds-quaternary font-bold">
-                      {`${window.location.protocol}//${window.location.host}/${room}`}
-                    </span>
-                  </p>
-                </div>
-                <div className="flex justify-center items-center">
-                  <QRCodeSVG
-                    value={`${window.location.protocol}//${window.location.host}/${room}`}
-                    size={256}
-                    className="bg-white p-2"
-                  />
-                </div>
-              </div>
-            )}
-
-            {gameStarted && currentQuote && (
-              <>
-                <p className="pt-16 pb-4 text-center text-4xl font-bold">
-                  &ldquo;{currentQuote.quote}&rdquo;
+      {!gameEndedAt && (
+        <>
+          {!gameStarted && (
+            <div className="self-center">
+              <div>
+                <p className="text-center text-2xl mb-8">
+                  Join the game at{" "}
+                  <span className="text-3xl text-ds-quaternary font-bold">
+                    {`${window.location.protocol}//${window.location.host}/${room}`}
+                  </span>
                 </p>
-                {isRoundDecided && (
-                  <div>
-                    <p className="text-center">The answer:</p>
-                    <p className="pb-4 text-center text-4xl font-bold">
-                      {currentQuote.options[currentQuote?.correctOptionIndex]}
-                    </p>
-                  </div>
-                )}
-                {!isRoundDecided && (
-                  <p className="text-center text-6xl font-bold">
-                    {timeRemaining / 1000}
-                  </p>
-                )}
-              </>
-            )}
-
-            {activeTeams.length > 0 && (
-              <div className="flex flex-col justify-end">
-                <h2 className="text-center">Leaderboard</h2>
-
-                <ol className="w-5/6 mx-auto">
-                  {activeTeams.map((team) => (
-                    <li
-                      key={team.id}
-                      className={`rounded text-center text-3xl font-bold bg-${
-                        teamBgColors[team.id]
-                      } mb-2 h-24 flex items-center p-4`}
-                    >
-                      <p className="flex-grow text-left">Team {team.id}</p>
-                      <p>
-                        {visibleScore(team, currentQuoteIndex, isRoundDecided)}{" "}
-                        pts
-                      </p>
-                    </li>
-                  ))}
-                </ol>
               </div>
-            )}
-          </>
-        )}
-      </main>
-      <pre>{JSON.stringify(state, null, 2)}</pre>
-    </>
+              <div className="flex justify-center items-center">
+                <QRCodeSVG
+                  value={`${window.location.protocol}//${window.location.host}/${room}`}
+                  size={256}
+                  className="bg-white p-2"
+                />
+              </div>
+            </div>
+          )}
+
+          {gameStarted && currentQuote && (
+            <>
+              <p className="pt-16 pb-4 text-center text-4xl font-bold">
+                &ldquo;{currentQuote.quote}&rdquo;
+              </p>
+              {isRoundDecided && (
+                <div>
+                  <p className="text-center">The answer:</p>
+                  <p className="pb-4 text-center text-4xl font-bold">
+                    {currentQuote.options[currentQuote?.correctOptionIndex]}
+                  </p>
+                </div>
+              )}
+              {!isRoundDecided && (
+                <p className="text-center text-6xl font-bold">
+                  {timeRemaining / 1000}
+                </p>
+              )}
+            </>
+          )}
+
+          {activeTeams.length > 0 && (
+            <div className="flex flex-col justify-end">
+              <h2 className="text-center">Leaderboard</h2>
+
+              <ol className="w-5/6 mx-auto">
+                {activeTeams.map((team) => (
+                  <li
+                    key={team.id}
+                    className={`rounded text-center text-3xl font-bold bg-${
+                      teamBgColors[team.id]
+                    } mb-2 h-24 flex items-center p-4`}
+                  >
+                    <p className="flex-grow text-left">Team {team.id}</p>
+                    <p>
+                      {visibleScore(team, currentQuoteIndex, isRoundDecided)}{" "}
+                      pts
+                    </p>
+                  </li>
+                ))}
+              </ol>
+            </div>
+          )}
+        </>
+      )}
+    </main>
   );
 }
