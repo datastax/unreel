73fc248d08f3979ea119335749d6824847d98c60
Tejas Kumar <tejas.kumar@datastax.com>
2024-11-13 20:38:51 -0500

    Update leaderboard

---
+++ ui/src/PhoneIcon.tsx
@@ -0,0 +1,19 @@
+export function PhoneIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect x="5" y="2" width="14" height="20" rx="2" ry="2" />
+      <line x1="12" y1="18" x2="12" y2="18" />
+      <line x1="9" y1="6" x2="15" y2="6" />
+    </svg>
+  );
+}
