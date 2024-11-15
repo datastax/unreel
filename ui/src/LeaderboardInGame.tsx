7b62dc936568fbecc7df85b30063d59345b32946
Tejas Kumar <tejas.kumar@datastax.com>
2024-11-15 13:06:46 -0500

    Update interstitial leaderboard

--- ui/src/LeaderboardInGame.tsx
+++ ui/src/LeaderboardInGame.tsx
@@ -21,15 +21,25 @@ export function LeaderboardInGame({
   timeRemaining: number;
 }) {
   const correctAnswer = currentQuote.options[currentQuote?.correctOptionIndex];
+  const teamWithBiggestNewScore = activeTeams
+    .map((team) => ({
+      ...team,
+      scoreDelta: team.previousRoundScore - team.score,
+    }))
+    .sort((a, b) => a.scoreDelta - b.scoreDelta)[0];
+  const teamWithBiggestScore = [...activeTeams].sort(
+    (a, b) => b.score - a.score
+  )[0];
 
   return (
     <LeaderboardLayout>
-      <div
-        className={`flex gap-8 items-start w-full ${
-          isRoundDecided ? "flex-col-reverse" : "flex-row"
-        }`}
-      >
+      <div className={`flex gap-8 items-start w-full`}>
         <div className="w-full grid gap-8">
+          {isRoundDecided && (
+            <h2 className="text-6xl font-bold">
+              Team {teamWithBiggestNewScore.id} won!
+            </h2>
+          )}
           <div className="grid gap-4 content-start text-left">
             <p className="text-2xl">This quote:</p>
             <p className="text-4xl font-bold">
@@ -70,7 +80,9 @@ export function LeaderboardInGame({
                 style={{ viewTransitionName: `team-${team.id}` }}
                 className={`rounded text-center overflow-hidden bg-${
                   teamBgColors[team.id]
-                } bg-opacity-35 grid gap-2 text-3xl font-bold relative h-24 p-4`}
+                } bg-opacity-35 grid gap-2 text-3xl font-bold relative ${
+                  team.id === teamWithBiggestScore.id ? "h-32" : "h-16"
+                } p-4`}
               >
                 <div
                   className={`absolute -z-1 h-full top-0 left-0 bg-${
