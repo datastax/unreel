52c1a05763fed37ad52bc7bfd45a843a72964849
Tejas Kumar <tejas.kumar@datastax.com>
2024-11-15 13:11:02 -0500

    Make crown white

--- ui/src/LeaderboardEndgame.tsx
+++ ui/src/LeaderboardEndgame.tsx
@@ -26,7 +26,7 @@ export function LeaderboardEndgame({
             <svg
               className="absolute -top-8 -left-6 w-16 h-16 -rotate-[30deg]"
               viewBox="0 0 24 24"
-              fill="currentColor"
+              fill="white"
             >
               <path d="M5 16L3 5L8.5 10L12 4L15.5 10L21 5L19 16H5ZM19 19C19 19.6 18.6 20 18 20H6C5.4 20 5 19.6 5 19V18H19V19Z" />
             </svg>
